1bad16b65dc22167a4e20410cf41ec5113dd28b7
Kourtney Queeley <Kourtney_queeley@hotmail.com>
2023-07-21 12:24:28 +0100

    Approutes added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Stack } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
 import { NavigationBar } from "./components/NavigationBar";
+import { AppRoutes } from "./AppRoutes";
 
 export const App = () => {
   return (
     <BrowserRouter>
       <NavigationBar />
+      <AppRoutes />
     </BrowserRouter>
   );
 };
--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,11 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { NavigationBar } from "./components/NavigationBar";
 import { Home } from "./pages/Home";
+import { MyRepos } from "./pages/MyRepos";
+import { NotFound } from "./pages/NotFound";
 
-export const App = () => {
+export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/myrepos" element={<MyRepos />} />
+      <Route path="" element={<NotFound />} />
     </Routes>
   );
 };
